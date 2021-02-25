5a4e9633b3550162f760cf70b91b5486d7de2126
boleth <74958282+boleth@users.noreply.github.com>
2021-02-25 11:23:42 +0100

    slideshow

---
+++ Slideshow/slideshow.js
@@ -0,0 +1,23 @@
+var i = 0; //Start point
+var images = [];
+var time = 3000;
+
+// Billede liste
+images[0] = "frisoer.jpg";
+images[1] = "frisoer1.jpg";
+images[2] = "frisoer2.jpg";
+
+// Billede skift
+function changeImg() {
+  document.slide.src = images[i];
+
+  if (i < images.length - 1) {
+    i++;
+  } else {
+    i = 0;
+  }
+
+  setTimeout("changeImg()", time);
+}
+
+window.onload = changeImg;
